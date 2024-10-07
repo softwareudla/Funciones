88caa0ac3253470b959b8241ffa2155a84847cf6
carlos.guaita <carlos.guaita@udla.edu.ec>
2024-10-07 17:46:02 -0500

    inicio3

--- test/test.sh
+++ test/test.sh
@@ -5,7 +5,7 @@
 
 # Caso 1: Ingreso de productos y precios válidos
 # Simulamos la entrada de datos con echo y redireccionamos la salida a un archivo
-echo -e "Producto1\n100\nProducto2\n200\nProducto3\n300\nProducto4\n400\nProducto5\n500\nProducto6\n600\nProducto7\n700\nProducto8\n800\nProducto9\n900\nProducto10\n1000" | ./inventario > salida.txt
+echo -e "Producto1\n100\nProducto2\n200\nProducto3\n300\nProducto4\n400\nProducto5\n500\nProducto6\n600\nProducto7\n700\nProducto8\n800\nProducto9\n900\nProducto10\n1000" | ./main > salida.txt
 
 # Verificamos si la salida es la esperada
 # En este caso el total del inventario debería ser 5500
